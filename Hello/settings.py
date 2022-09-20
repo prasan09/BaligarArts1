f5c58758fd11b9ba27534bee03786d35e10e0077
Prasan Baligar <94674463+prasan09@users.noreply.github.com>
2022-09-20 15:40:51 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6w4kbygqfo@11!j2u0!s!ll&qme1kjs6!@0te!kb4_zua6!fjf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["baligararts.herokuapp.com/"]
 
